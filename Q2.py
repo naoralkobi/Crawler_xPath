9b45a67f2a92a86fa16794fe358cf710f423abdc
naoralkobi <naor9985@gmail.com>
2022-11-24 20:29:24 +0200

    finish Q2

---
+++ Q2.py
@@ -0,0 +1,28 @@
+import requests
+import lxml.html
+import lxml.etree
+
+
+def is_member(url):
+    response = requests.get(url)
+    html = response.text
+    doc = lxml.html.fromstring(html)
+
+    x_path = "//div[@class='mw-body-content mw-content-ltr']//p//text()" \
+             "[contains(.,'Queen of the United Kingdom') " \
+             "or contains(.,'British royal family') or contains(.,'King of the United Kingdom')]"
+
+    return doc.xpath(x_path)
+
+
+if __name__ == '__main__':
+    print(is_member("https://en.wikipedia.org/wiki/Elizabeth_II"))
+    print(is_member("https://en.wikipedia.org/wiki/Football"))
+    print(is_member("https://en.wikipedia.org/wiki/George_VI"))
+    print(is_member("https://en.wikipedia.org/wiki/Queen_Elizabeth_The_Queen_Mother"))
+    print(is_member("https://en.wikipedia.org/wiki/Prince_Henry,_Duke_of_Gloucester"))
+    print(is_member("https://en.wikipedia.org/wiki/Princess_Alice,_Duchess_of_Gloucester"))
+    print(is_member("https://en.wikipedia.org/wiki/Prince_George,_Duke_of_Kent"))
+    print(is_member("https://en.wikipedia.org/wiki/Princess_Marina_of_Greece_and_Denmark"))
+    print(is_member("https://en.wikipedia.org/wiki/George_V"))
+    print(is_member("https://en.wikipedia.org/wiki/Mary_of_Teck"))
--- main.py
+++ main.py
@@ -1,18 +1,35 @@
 import requests
 import lxml.html
+import lxml.etree
 
-URL = "https://en.wikipedia.org/wiki/Charles_III"
+URL2 = "https://en.wikipedia.org/wiki/Charles_III"
+URL1 = "https://en.wikipedia.org/wiki/Prince_Henry,_Duke_of_Gloucester"
+URL = "https://en.wikipedia.org/wiki/Elizabeth_II"
 
 if __name__ == '__main__':
     # get the html of the page
     response = requests.get(URL)
     html = response.text
     doc = lxml.html.fromstring(html)
-    keywords = ["I", "II", "III", "V", "IV", "VI", "VII", "VIII"]
+    x_path_list = ["//table[@class='infobox vcard']//th[contains(text(), 'Father') or contains(text(), 'Mother')]/..//@href[contains(.,'/wiki/')]",
 
-    for word in keywords:
-        for url in doc.xpath("//a/@href[contains(.,'%s')]" % word):
-            print(url)
+                   "//p[contains(text(), 'sibling') or contains(text(), 'sister') or contains(text(), 'brother')]/a[contains(text(), 'Prince')]//@href[contains(.,'/wiki/')]",
 
+                    "//table[@class='infobox vcard']//a[contains(text(), 'Prince') or contains(text(), 'King') or contains(text(), 'II')]//@href[contains(.,'/wiki/')]",
 
+                   "//table[@class ='wikitable plainrowheaders']//td[4]//@href[contains(.,'/wiki/')]"]
 
+    # for x_path in x_path_list:
+    #     for url in doc.xpath(x_path):
+    #         print(url)
+
+    x_path1 = "//table[@class='wikitable plainrowheaders']//td//@href[contains(.,'/wiki/')]"
+    x_path2 = "//table[@class='ahnentafel']//td//@href[contains(.,'/wiki/')]"
+    x_path = "//div[@class='mw-body-content mw-content-ltr']//p//text()" \
+             "[contains(.,'Queen of the United Kingdom') " \
+             "or contains(.,'British royal family') or contains(.,'King of the United Kingdom')]"
+    # x_path = "//div[@class='mw-body-content mw-content-ltr']//p//text()"
+    for url in doc.xpath(x_path):
+        print(url)
+# "//table[@class='infobox vcard']//th[contains(text(), 'Spouse')]/..//@href[contains(.,'/wiki/')]",
+# "//table[@class='infobox vcard']//th[contains(text(), 'Issue')]//..//@href[contains(.,'/wiki/')]",
