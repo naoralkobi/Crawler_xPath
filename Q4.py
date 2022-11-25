ecb8d79efc068477064c331845a4368d7d9a3009
Ben Eli <benelashvili@gmail.com>
2022-11-25 19:18:45 +0200

    q4 #TODO -  valid_family_members

--- Q4.py
+++ Q4.py
@@ -1,13 +1,19 @@
 
+def intersection(lst1, lst2):
+    return list(set(lst1) & set(lst2))
+
 def crawlerQuality(listOfPairs):
     answer = dict()
-    valid_finds = []
+    valid_finds = list()
+    number_of_crawls = 0
     for i in listOfPairs:
         if i[2] == 1:
+            number_of_crawls += 1
             if i[1] in valid_family_members:
                 valid_finds.append(i[1])
-    answer["precision"] = valid_finds / len(listOfPairs)
-    answer["recall"] = valid_finds / 43
+    inter = len(intersection(valid_find,valid_family_members))
+    answer["precision"] = inter / number_of_crawls
+    answer["recall"] = inter / (len(valid_family_members)-1)
     answer["F1"] = 2 * answer["precision"] * answer["recall"] / (answer["precision"] + answer["recall"])
     return answer
 
