16ac4d73a6781a63661f886d550a03acbded4076
Ben Eli <benelashvili@gmail.com>
2022-11-25 18:53:41 +0200

    working on q4

--- Q4.py
+++ Q4.py
@@ -1,13 +1,14 @@
-import requests
-import lxml.html
-import lxml.etree
 
 def crawlerQuality(listOfPairs):
     answer = dict()
-    valid_finds = 0
+    valid_finds = []
     for i in listOfPairs:
-        if i[2]==0:
-            valid_finds+=1
-    answer["precision"]=valid_finds/len(listOfPairs)
-    answer["recall"]=valid_finds/43
-    answer["F1"]=2*answer["precision"]*answer["recall"]/(answer["precision"]+ answer["recall"])
+        if i[2] == 1:
+            if i[1] in valid_family_members:
+                valid_finds.append(i[1])
+    answer["precision"] = valid_finds / len(listOfPairs)
+    answer["recall"] = valid_finds / 43
+    answer["F1"] = 2 * answer["precision"] * answer["recall"] / (answer["precision"] + answer["recall"])
+    return answer
+
+
